6339c8446024d6971daef54bffcd6c81b823d711
Luke <Lukecam07@hotmail.com>
2018-09-04 19:11:26 +1000

    Home page on django

--- DatabaseRework/DatabaseRework/settings.py
+++ DatabaseRework/DatabaseRework/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -52,10 +52,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'DatabaseRework.urls'
 
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [PROJECT_PATH + '/page'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
--- DatabaseRework/DatabaseRework/views.py
+++ DatabaseRework/DatabaseRework/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<p><b>Login details for <a href=\"/admin/\">/admin/</b></a></p>"
-                        "<p><b>Username:</b> admin</p>"
-                        "<p><b>Password:</b> password</p>")
+    return render(request,'index.html')
+
+    #return HttpResponse("<p><b>Login details for <a href=\"/admin/\">/admin/</b></a></p>"
+     #                   "<p><b>Username:</b> admin</p>"
+      #                  "<p><b>Password:</b> password</p>")
