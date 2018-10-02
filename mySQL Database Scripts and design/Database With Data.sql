1ab4f304d4b3193901910e65015da8e778c480e3
Luke <Lukecam07@hotmail.com>
2018-10-02 18:15:16 +1000

    Updated mySQL data import script

--- mySQL Database Scripts and design/Database With Data.sql
+++ mySQL Database Scripts and design/Database With Data.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `backend` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `backend`;
 -- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: backend
@@ -444,4 +446,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-09-19 18:26:33
+-- Dump completed on 2018-10-02 18:14:51
