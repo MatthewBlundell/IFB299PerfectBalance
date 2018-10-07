1bf72c401702537623071924090cbc88125f7a2a
Tom Walker <tomwalker@MAC72.local>
2018-10-07 13:16:07 +1000

    Organising Meeting Logs

---
+++ DatabaseRework/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/tomwalker/Desktop/ifb299perfectbalance/DatabaseRework/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
