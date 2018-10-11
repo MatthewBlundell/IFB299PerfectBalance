4d8302578f3f0177eb597071bae4545beeabc512
Matthew Blundell <matthew.blundell@outlook.com>
2018-10-11 20:04:11 +1000

    Updated css and html for the signup page

--- Pages Directory/Updated Login Page/main.js
+++ Pages Directory/Updated Login Page/main.js
@@ -3,30 +3,55 @@
 // document ready function
 $(function(){
     
-    // global configurations
-    var width = 1280;
-    var animationSpeed = 1000;
-    var pause = 5000;
-    var currentSlide = 1;
+    // going to start coding for login form only appeariong once login button has been pressed
 
-    // cache the DOM
-    var $slider = $('#slider');
-    var $slideContainer = $slider.find('.slides');
-    var $slides = $slideContainer.find('.slide');
+    //setting up global variable and configs
 
-    //setInterval for image slider
-    setInterval(function(){
-        //animate the images by animating margin-left
-        $slideContainer.animate({'margin-left': '-=' + width}, animationSpeed, function(){
-            //if the slides reach last slide then go back to beginning
-            currentSlide++;
-            console.log(currentSlide);
-            if(currentSlide === $slides.length){
-                currentSlide = 1;
-                $slideContainer.css('margin-left', 0);
-            }
-        });
-    }, pause);
+    var arrow = $(".arrow");
+    var form = $(".login-form");
+    var status =  false;
+    var disable = $(".wrapper");
+    var scroll = $("body");
+    console.log(scroll);
+    
+    $(".submit").click(function(event){
+        event.preventDefault();
+        if(status == false){
+            arrow.fadeIn();
+            form.fadeIn();
+            form.css({'z-index': '3', 'opacity': '1'});
+            disable.css({'z-index': '2', 'opacity': '0.6'});       
+            status = true;
+        }
+        else{
+            arrow.fadeOut();
+            form.fadeOut();
+            form.css({'z-index': '0', 'opacity': '1'});
+            disable.css({'z-index': '0', 'opacity': '0'}); 
+            scroll.css({'overflow': 'visible'});
+            status = false;
+        }
+    })
+
+    $(".loginbtn").click(function(event){
+        event.preventDefault();
+        if(status == false){
+            arrow.fadeIn();
+            form.fadeIn();
+            form.css({'z-index': '3', 'opacity': '1'});
+            disable.css({'z-index': '2', 'opacity': '0.6'});
+            scroll.css({'overflow': 'hidden'});               
+            status = true;
+        }
+        else{
+            arrow.fadeOut();
+            form.fadeOut();
+            form.css({'z-index': '0', 'opacity': '1'});
+            disable.css({'z-index': '0', 'opacity': '0'});
+            scroll.css({'overflow': 'visible'}); 
+            status = false;
+        }
+    })
 });
 
 function toggleSidebar(){
