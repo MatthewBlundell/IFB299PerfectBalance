61cb0568ff7db2765c56210b22c00abac6d44cc8
Luke <Lukecam07@hotmail.com>
2018-10-16 10:09:44 +1000

    Searching filter has been set up

--- DatabaseRework - Django Server/LoginRegister/views.py
+++ DatabaseRework - Django Server/LoginRegister/views.py
@@ -1,12 +1,46 @@
 from django.template import loader
 from Search.models import User
 from django.http import HttpResponse, HttpResponseRedirect
+import re
 
+def CheckString(input):
+    pass
 
+def CheckName(input):
+    m = re.search(r'[A-Za-z]+ [A-za-z]+', input)
+    if m:
+        return True
+    else:
+        return False
+
+def CheckInt(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+
+    return False
+
+def CheckDate(input):
+    pass
+
+def CheckUser(input):
+    pass
+
+def CheckPass(input):
+    pass
 
 def Loginenter(request):
     Check = False
-
+    NameVerify = True
     if request.session.has_key('email'):
         return HttpResponseRedirect('http://127.0.0.1:8000/');
 
@@ -17,14 +51,25 @@ def Loginenter(request):
     registerverify = True
     registerexists = False
     if request.method == "POST":
-        pass
-
+        Name = request.POST.get('Name')
+        Number = request.POST.get('Number')
+        Address = request.POST.get('Address')
+        DOB = request.POST.get('DOB')
+        Occupation = request.POST.get('Occupation')
+        Gender = request.POST.get('Gender')
+        Email = request.POST.get('Email')
+        Password = request.POST.get('Password')
+        PasswordRe = request.POST.get('PasswordRe')
+        NameVerify = CheckName(Name)
+        NumberVerify = CheckInt(Number)
     template = loader.get_template('RegisterForm.html')
     context = {
         'register': registerverify,
         'exists': registerexists,
         'Check': Check,
         'session': request.session.has_key('email'),
+        'NameVerify': NameVerify,
+        'NumberVerify': NumberVerify,
     }
     return HttpResponse(template.render(context, request))
 
--- DatabaseRework - Django Server/SearchResults/views.py
+++ DatabaseRework - Django Server/SearchResults/views.py
@@ -2,6 +2,16 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from Search.models import User, Vehicle
 
+Brand = ['BMW', 'VOLKSWAGEN', 'MERCEDES-BENZ', 'MAZDA', 'DATSUN', 'ALFA ROMEO', 'VOLVO', 'RENAULT', 'LAND ROVER',
+         'SAAB', 'NISSAN', 'PEUGOT', 'CHRYSLER']
+
+Body = ['4D WAGON', '3D HARDBACK', '2D HARDBACK', '2D HARDTOP', '3D HATCHBACK', '4D SEDAN', '2D COUPE']
+
+Fuel = ['DIESEL TURBO F/INJ', 'DIESEL TURBO', 'MULTI POINT F/INJ', 'TURBO CDI', 'ELECTRONIC F/INJ', 'CARB',
+        'SINGLE POINT F/INJ', 'TURBO MPFI', 'SUPER CHARGED MPFI']
+
+Seats = [2, 3, 4, 5, 6, 7]
+
 
 def search(request):
     Check = False
@@ -30,6 +40,23 @@ def search(request):
     else:
         VehicleList = StringSearch(SearchQuery.lower())
 
+    Filters = [0, 0, 0, 0, 0, 0]
+
+
+    Brand = request.GET.get('Brand')
+    Body = request.GET.get('Body')
+    Seats = request.GET.get('Seats')
+    Fuel = request.GET.get('Fuel')
+    Min = request.GET.get('Min')
+    Max = request.GET.get('Max')
+
+
+    if Brand != "None" and Body != "None" and Seats != "None" and Fuel != "None" and Min != "None" and Max != None:
+        Filters = [int(Brand), int(Body), int(Seats), int(Fuel), int(Min), int(Max)]
+        VehicleList = FilterSearch(VehicleList, Filters)
+
+    DupeRemover(VehicleList)
+
     template = loader.get_template('SearchPage.html')
     context = {
         'Search': SearchQuery,
@@ -40,6 +67,12 @@ def search(request):
         'authlevel': auth,
         'Check': Check,
         'session': request.session.has_key('email'),
+        'Brand': Filters[0],
+        'Body': Filters[1],
+        'Seats': Filters[2],
+        'Fuel': Filters[3],
+        'Min': Filters[4],
+        'Max': Filters[5],
     }
     return HttpResponse(template.render(context, request))
 
@@ -64,15 +97,16 @@ def StringSearch(input):
     inputsplit = input.split()
     vehiclelist = []
 
-    if 'land' in inputsplit:
-        if 'rover' in inputsplit:
-            for i in Vehicle.objects.filter(carmake="LAND ROVER"):
-                vehiclelist.append(i)
-
-    if 'alfa' in inputsplit:
-        if 'romeo' in inputsplit:
-            for i in Vehicle.objects.filter(carmake="ALFA ROMEO"):
-                vehiclelist.append(i)
+    for i in Vehicle.objects.filter(carmake=r'' + input):
+        vehiclelist.append(i)
+    for i in Vehicle.objects.filter(model=r'' + input):
+        vehiclelist.append(i)
+    for i in Vehicle.objects.filter(series=r'' + input):
+        vehiclelist.append(i)
+    for i in Vehicle.objects.filter(standardtransmission=r'' + input):
+        vehiclelist.append(i)
+    for i in Vehicle.objects.filter(carbodytype=r'' + input):
+        vehiclelist.append(i)
 
     for z in inputsplit:
         for i in Vehicle.objects.filter(carmake=z):
@@ -89,20 +123,98 @@ def StringSearch(input):
         for i in Vehicle.objects.filter(carbodytype=z):
             vehiclelist.append(i)
 
-    output = []
-    seen = set()
-    for value in vehiclelist:
-        if value not in seen:
-            output.append(value)
-            seen.add(value)
+    return vehiclelist
 
-    return output
+def CheckFilters(filters, temp2):
+    temp = temp2
+    if filters[0] > 0:
+        for i in range(len(temp)-1, -1, -1):
+            if temp[i].carmake != Brand[filters[0]-1]:
+                del temp[i]
 
-def FilterSearch(input):
-    pass
+    if filters[1] > 0:
+        for i in range(len(temp)-1, -1, -1):
+            if temp[i].carbodytype != Body[filters[1]-1]:
+                del temp[i]
+
+    if filters[2] > 0:
+        if filters[2] == 6:
+            for i in range(len(temp)-1, -1, -1):
+                allchecks = True
+                for z in range(7,20):
+                    if temp[i].seatingcapacity == z:
+                        allchecks = False
+                    if allchecks == True:
+                        del temp[i]
+
+        else:
+            for i in range(len(temp)-1, -1, -1):
+                if temp[i].seatingcapacity != Seats[filters[2]-1]:
+                    del temp[i]
+
+    if filters[3] > 0:
+        for i in range(len(temp)-1, -1, -1):
+            if temp[i].fuelsystem != Fuel[filters[3]-1]:
+                del temp[i]
+
+    if filters[4] > 0 or filters[5] > 0:
+        for i in range(len(temp)-1, -1, -1):
+            if temp[i].price < filters[4] or temp[i].price > filters[5]:
+                del temp[i]
+
+    return temp
+
+
+def FilterSearch(input, filters):
+    temp = CheckFilters(filters, input)
+
+    temp2 = []
+    if filters[0] > 0:
+        for i in Vehicle.objects.filter(carmake=Brand[filters[0]-1]):
+            temp2.append(i)
+
+    if filters[1] > 0:
+        for i in Vehicle.objects.filter(carbodytype=Body[filters[1]-1]):
+            temp2.append(i)
+
+    if filters[2] > 0:
+        if filters[2] == 6:
+            for i in Vehicle.objects.filter(seatingcapacity=[7, 8, 9, 10, 11, 12, 13, 14, 15, 16]):
+                temp2.append(i)
+        else:
+            for i in Vehicle.objects.filter(seatingcapacity=Seats[filters[2]-1]):
+                temp2.append(i)
+
+    if filters[3] > 0:
+        for i in Vehicle.objects.filter(fuelsystem=Fuel[filters[3]-1]):
+            temp2.append(i)
+
+    if filters[4] > 0:
+        for i in Vehicle.objects.filter(price__lte=filters[4]):
+            temp2.append(i)
+
+    if filters[5] > 0:
+        for i in Vehicle.objects.filter(price__gte=filters[5]):
+            temp2.append(i)
+
+    temp2 = CheckFilters(filters, temp2)
+
+    for i in temp2:
+        temp.append(i)
+
+    return temp
 
 def AvaibilitySearch(input):
     pass
 
 def returner(request):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'));
+
+def DupeRemover(input):
+    output = []
+    seen = set()
+    for value in input:
+        if value not in seen:
+            output.append(value)
+            seen.add(value)
+    return output
